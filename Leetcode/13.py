0763eb28ce5acbc054cef02d291a6f2dd8435550
YIZHI SUN <syzuser60@gmail.com>
2019-05-17 13:26:57 -0400

    add 13.py

---
+++ Leetcode/13.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/roman-to-integer/
+# https://www.youtube.com/watch?v=MBfPfAH6jdE
+# T: O(n)
+# S: O(1)
+
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        result = 0
+        number_dict = {'I':1, 'V':5, 'X':10, 'L':50,
+                       'C':100, 'D':500, 'M':1000}
+        
+        for i in range(len(s)):
+            if i > 0 and number_dict[s[i]] > number_dict[s[i - 1]]:
+                result += number_dict[s[i]] - 2*number_dict[s[i - 1]]
+                
+            else:
+                result += number_dict[s[i]]
+                
+        return result
