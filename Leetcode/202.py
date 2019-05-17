594417deb898f9657c55b0fb85d1becfb8f6dd56
YIZHI SUN <syzuser60@gmail.com>
2019-05-17 15:01:43 -0400

    add 202.py

---
+++ Leetcode/202.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/happy-number/
+
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        d = set()
+        while n != 1:
+            n = sum([int(i)**2 for i in str(n)])
+            if n in d:
+                return False
+            else:
+                d.add(n)
+                
+        return True
