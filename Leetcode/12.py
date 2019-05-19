9a25e6774d766348d781c17c8c0c13724e590e1f
YIZHI SUN <syzuser60@gmail.com>
2019-05-19 06:15:55 -0400

    add 12.py

---
+++ Leetcode/12.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/integer-to-roman/
+# T: O(n)
+# S: O(1)
+
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        
+        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        numerals = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+        
+        result = ''
+        for i in range(len(values)):
+            while num >= values[i]:
+                num -= values[i]
+                result += numerals[i]
+        return result
