f4db3032aa0e3f34b4c8f40a3613330a197e889b
YIZHI SUN <syzuser60@gmail.com>
2019-05-23 13:51:24 -0400

    add 24.py

---
+++ Leetcode/24.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/swap-nodes-in-pairs/
+# T: O(n)
+# S: O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        dummy = ListNode(0)
+        dummy.next = head
+        p = dummy
+        while p.next and p.next.next:
+            tmp = p.next.next
+            p.next.next = tmp.next
+            tmp.next = p.next
+            p.next = tmp
+            p = p.next.next
+            
+        return dummy.next
+        
