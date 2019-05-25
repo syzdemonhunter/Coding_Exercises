f1074cb908f4e3b46ffd83803df78d8ffaacb378
YIZHI SUN <syzuser60@gmail.com>
2019-05-25 15:21:28 -0400

    modify 75.py

--- Leetcode/75.py
+++ Leetcode/75.py
@@ -1,12 +1,11 @@
-# https://algocasts.io/episodes/aVWyAYW2
+# https://leetcode.com/problems/sort-colors/
 # T: O(n)
 # S: O(1)
 
-class Solution(object):
-    def sortColors(self, nums):
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
         """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
         if not nums or len(nums) == 0:
             return 
@@ -35,27 +34,3 @@ class Solution(object):
             nums[k] = 2
             k += 1
             l += 1
-            
-
-#### or #####
-class Solution(object):
-    def sortColors(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
-        if not nums or len(nums) == 0:
-            return 
-        
-        left = mid = 0
-        right = len(nums) - 1
-        while mid <= right:
-            if nums[mid] == 0:
-                nums[mid], nums[left] = nums[left], nums[mid]
-                mid += 1
-                left += 1
-            elif nums[mid] == 1:
-                mid += 1
-            else:
-                nums[right], nums[mid] = nums[mid], nums[right]
-                right -= 1
