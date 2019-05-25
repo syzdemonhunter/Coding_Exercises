fe76c66e39ae7852838f2bec4509af0c5681e8ae
YIZHI SUN <syzuser60@gmail.com>
2019-05-25 15:04:52 -0400

    add 165.py

---
+++ Leetcode/165.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/compare-version-numbers/
+# T: O(n)
+# S: O(n)
+
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        v1_list = version1.split('.')
+        v2_list = version2.split('.')
+        
+        for i in range(max(len(v1_list), len(v2_list))):
+            v1 = int(v1_list[i]) if len(v1_list) > i else 0
+            v2 = int(v2_list[i]) if len(v2_list) > i else 0
+            if v1 > v2:
+                return 1
+            elif v1 < v2:
+                return -1
+        return 0
+        
