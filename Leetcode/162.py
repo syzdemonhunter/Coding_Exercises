0c87a6a805668ed8c84d400ee3878c30037a8f30
YIZHI SUN <syzuser60@gmail.com>
2019-05-25 23:25:16 -0700

    add 162.py

---
+++ Leetcode/162.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/find-peak-element/
+# T: O(n)
+# S: O(1)
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        start, end = 0, len(nums) - 1
+        
+        while start < end:
+            mid = start + (end - start)//2
+            if nums[mid] < nums[mid + 1]:
+                start = mid + 1
+            else:
+                end = mid
+                
+        return start
+            
+        
