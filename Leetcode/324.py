491b0a6b41488287bc41c5fb20283ff5ebeb8e2e
YIZHI SUN <syzuser60@gmail.com>
2019-05-28 14:55:24 -0700

    add 324.py

---
+++ Leetcode/324.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/wiggle-sort-ii/
+# https://leetcode.com/problems/wiggle-sort-ii/discuss/77678/3-lines-Python-with-Explanation-Proof
+# T: O(nlogn)
+# S: O(n)
+
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort()
+        half = len(nums[::2]) - 1
+        nums[::2], nums[1::2] = nums[half::-1], nums[:half:-1]
