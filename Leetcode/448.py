1b2d39e63d2df8c089ae338b6f5214e65bca3068
YIZHI SUN <syzuser60@gmail.com>
2019-05-29 14:55:29 -0700

    modify 448.py

--- Leetcode/448.py
+++ Leetcode/448.py
@@ -2,14 +2,24 @@
 # T: O(n)
 # S: O(1)
 
-class Solution:
-    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        for i in range(len(nums)):
-            n = abs(nums[i])
-            nums[n - 1] = -abs(nums[n - 1])
-            
+
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        if not nums:
+            return []
         result = []
+        
         for i in range(len(nums)):
-            if nums[i] > 0:
-                result.append(i + 1)
+            idx = abs(nums[i]) - 1
+            if nums[idx] > 0:
+                nums[idx] = -nums[idx]
+                
+        for j in range(len(nums)):
+            if nums[j] > 0:
+                result.append(j + 1)
+                
         return result
