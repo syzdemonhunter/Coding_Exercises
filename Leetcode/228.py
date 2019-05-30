04e071730ed3a7d4e68a62cd27a88ffd1d881acb
YIZHI SUN <syzuser60@gmail.com>
2019-05-29 20:31:06 -0700

    add 228.py

---
+++ Leetcode/228.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/summary-ranges/
+# T: O(n)
+# S: O(1)
+
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        if not nums:
+            return []
+        
+        result = []
+        start, end = nums[0], nums[0]
+        for i in range(1, len(nums) + 1):
+            if i < len(nums) and nums[i] == end + 1:
+                end = nums[i]
+            else:
+                interval = str(start)
+                if start != end:
+                    interval += '->' + str(end)
+                result.append(interval)
+                if i < len(nums):
+                    start = end = nums[i]
+                    
+        return result
+        
