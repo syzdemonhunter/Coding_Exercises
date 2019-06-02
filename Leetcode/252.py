381e97ac661b1e2deb6502d22a5199854f0b7d3a
YIZHI SUN <syzuser60@gmail.com>
2019-06-01 22:32:30 -0700

    add 252.py

---
+++ Leetcode/252.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/meeting-rooms/
+# T: O(nlogn)
+# S: O(1)
+
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        intervals.sort(key=lambda x: x[0])
+        for i in range(1, len(intervals)):
+            if intervals[i - 1][1] > intervals[i][0]:
+                return False
+            
+        return True
+        
