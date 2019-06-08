616af21245075274c6a7865c8987598efacb2e3b
YIZHI SUN <syzuser60@gmail.com>
2019-06-07 21:31:38 -0700

    add 206.py

--- Leetcode/206.py
+++ Leetcode/206.py
@@ -1,24 +1,32 @@
 # https://leetcode.com/problems/reverse-linked-list/
 # T: O(n)
 # S: O(1)
+
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-
-class Solution(object):
-    def reverseList(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        cur = head
-        pre = None
-        while cur is not None:
-            cur_next = cur.next
-            cur.next = pre
-            pre = cur
-            cur = cur_next
-        return pre
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        cur_node = head
+        prev_node = None
+        
+        while cur_node:
+            # Store the next node in a temporary variables
+            next_node = cur_node.next
+            
+            # Reverse the link
+            cur_node.next = prev_node
+            
+            #Update the previous node to the current node
+            prev_node = cur_node
+            
+            # Proceed to the next node in the original linked list
+            cur_node = next_node
+            
+        # Once the linked list has been reversed, prev_node will be
+        # referring to the new head. So return it.
+        return prev_node
+        
