3fa9e342ed9eb4946ef071baed075c8615028a79
YIZHI SUN <syzuser60@gmail.com>
2019-06-22 12:33:46 -0700

    add 282.py

---
+++ Leetcode/282.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/expression-add-operators/
+# T: ???
+# S: O(n)
+
+class Solution:
+    def addOperators(self, num: str, target: int) -> List[str]:
+        if not num or len(num) == 0:
+            return []
+        
+        result = []
+        self.target = target
+        for i in range(1, len(num)+1):
+            if i == 1 or (i > 1 and num[0] != "0"): # prevent "00*" as a number
+                self.dfs(num[i:], num[:i], int(num[:i]), int(num[:i]), result) # this step put first number in the string
+        return result
+
+    def dfs(self, num, temp, cur, last, res):
+        if not num:
+            if cur == self.target:
+                res.append(temp)
+            return
+        
+        for i in range(1, len(num) + 1):
+            val = num[:i]
+            if i == 1 or (i > 1 and num[0] != "0"): # prevent "00*" as a number
+                self.dfs(num[i:], temp + "+" + val, cur + int(val), int(val), res)
+                self.dfs(num[i:], temp + "-" + val, cur - int(val), -int(val), res)
+                self.dfs(num[i:], temp + "*" + val, cur - last + last*int(val), last*int(val), res)
+
+        
