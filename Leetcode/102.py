b6f41c4e91ab5cb5e7f714afe42f8e8d42c12e45
YIZHI SUN <syzuser60@gmail.com>
2019-06-21 20:09:59 -0700

    update 102.py

--- Leetcode/102.py
+++ Leetcode/102.py
@@ -1,40 +1,57 @@
-# https://leetcode.com/problems/binary-tree-level-order-traversal/
-# T: O(n)
-# S: O(n)
-
-
+# https://leetcode.com/problems/binary-tree-level-order-traversal/submissions/
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
-class Solution(object):
-    def levelOrder(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[int]]
-        """
+'''
+# T: O(n)
+# S: O(n)
+import collections
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
         if not root:
             return []
         
-        result, q = [], [root]
+        result = []
+        q = collections.deque([root])
         while q:
-            q_len = len(q)
-            i = 0
-            tmp_result = []
-            
-            while i < q_len:
-                node = q.pop(0)
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-                tmp_result.append(node.val)
-                i += 1
-            result.append(tmp_result)
+            size = len(q)
+            level = []
+            for i in range(size):
+                cur = q.popleft()
+                if cur.left:
+                    q.append(cur.left)
+                if cur.right:
+                    q.append(cur.right)
+                level.append(cur.val)
+                
+            result.append(level)
             
         return result
+'''
+# T: O(n)
+# S: O(n)
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
         
+        result = []
+        self.helper(result, root, 0)
+        return result
+    
+    def helper(self, result, root, level):
+        if not root:
+            return
+        if level >= len(result):
+            result.append([])
+        result[level].append(root.val)
+        self.helper(result, root.left, level + 1)
+        self.helper(result, root.right, level + 1)
+    
+    
+            
         
