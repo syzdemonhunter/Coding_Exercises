67b4579446a5d9f554634c6237294c849f894cf5
YIZHI SUN <syzuser60@gmail.com>
2019-06-22 13:09:17 -0700

    modify 322, 518

--- Leetcode/322.py
+++ Leetcode/322.py
@@ -1,22 +1,27 @@
 # https://leetcode.com/problems/coin-change/
-# space O(amount * len coins) , Time O(amount)
+# T: O(n*amount)
+# S: O(amount)
 
-class Solution(object):
-    def coinChange(self, coins, amount):
-        """
-        :type coins: List[int]
-        :type amount: int
-        :rtype: int
-        """
-        MAX = 2**32
-        dp = [0] + amount * [MAX]
-        for i in range(1, len(coins) + 1):
-            for j in range(coins[i - 1], amount + 1):
-                if dp[j - coins[i - 1]] != MAX:
-                    dp[j] = min(dp[j], dp[j - coins[i - 1]] + 1)
-        
-        if dp[amount] == MAX:
+# dp[]: 当前钱数需要多少coins
+# min_val = min(min_val, dp[i - coins[j]] + 1)
+# dp[5] = 3
+# coins[j] = 2
+# dp[7] = dp[7 - 2] + 1 = dp[5] + 1 = 4
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0:
+            return 0
+        if not coins or len(coins) == 0:
             return -1
-        else:
-            return dp[amount]
+        
+        dp = [0]*(amount + 1)
+        for i in range(1, amount + 1):
+            min_val = float('inf')
+            for j in range(len(coins)):
+                if i >= coins[j] and dp[i - coins[j]] != -1:
+                     min_val = min(min_val, dp[i - coins[j]] + 1)          
+            dp[i] = -1 if min_val == float('inf') else min_val
+            
+        return dp[amount]
         
--- Leetcode/518.py
+++ Leetcode/518.py
@@ -1,20 +1,15 @@
-# https://www.youtube.com/watch?v=jaNZ83Q3QGc&t=530s
 # https://leetcode.com/problems/coin-change-2/
-# T: O(amount*len(coins))
-# S: O(amount*len(coins))
+# T: O(m*n)
+# S: O(m)
 
-class Solution(object):
-    def change(self, amount, coins):
-        """
-        :type amount: int
-        :type coins: List[int]
-        :rtype: int
-        """
-        combinations = [0] * (amount + 1)
-        combinations[0] = 1
-
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0]*(amount + 1)
+        dp[0] = 1
+        
         for coin in coins:
             for i in range(coin, amount + 1):
-                combinations[i] += combinations[i - coin]
-
-        return combinations[amount]
+                dp[i] += dp[i - coin]
+                
+        return dp[amount]
+        
